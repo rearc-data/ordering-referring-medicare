e00ff8b3f7463043da456241bc65c46b0f379b16
Ayush Varma <avarma6@illinois.edu>
2020-07-09 14:41:24 -0500

    Revert code back for AWS

--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -4,16 +4,16 @@ from urllib.request import urlopen
 from urllib.error import URLError, HTTPError
 from multiprocessing.dummy import Pool
 
+
 def data_to_s3(frmt):
     # throws error occured if there was a problem accessing data
-	# otherwise downloads and uploads to s3
+    # otherwise downloads and uploads to s3
 
     source_dataset_url = 'https://data.cms.gov/api/views/qcn7-gc3g/rows'
-   
-    
+
     try:
         response = urlopen(source_dataset_url + frmt)
-        
+
     except HTTPError as e:
         raise Exception('HTTPError: ', e.code, frmt)
 
@@ -21,48 +21,44 @@ def data_to_s3(frmt):
         raise Exception('URLError: ', e.reason, frmt)
 
     else:
-      #  data_set_name = os.environ['DATA_SET_NAME']
-        data_set_name = 'yoyoyo123'
+        data_set_name = os.environ['DATA_SET_NAME']
         filename = data_set_name + frmt
-        file_location = 'C:/Users/Ayush Varma/Desktop/' + filename
-        
-        
+        file_location = '/tmp/' + filename
 
         with open(file_location, 'wb') as f:
-            print('hi')
             f.write(response.read())
             f.close()
 
         # variables/resources used to upload to s3
-        # s3_bucket = os.environ['S3_BUCKET']
-        # new_s3_key = data_set_name + '/dataset/'
-        # s3 = boto3.client('s3')
+        s3_bucket = os.environ['S3_BUCKET']
+        new_s3_key = data_set_name + '/dataset/'
+        s3 = boto3.client('s3')
 
-        # s3.upload_file(file_location, s3_bucket, new_s3_key + filename)			
-        
+        s3.upload_file(file_location, s3_bucket, new_s3_key + filename)
+
         print('Uploaded: ' + filename)
-        
-        
 
         # deletes to preserve limited space in aws lamdba
-        #os.remove(file_location)
+        os.remove(file_location)
 
         # dicts to be used to add assets to the dataset revision
-        #return {'Bucket': s3_bucket, 'Key': new_s3_key + filename}
-        
+        return {'Bucket': s3_bucket, 'Key': new_s3_key + filename}
+
 
 def source_dataset():
 
-	# list of enpoints to be used to access data included with product
-	data_endpoints = [
-		'.json',
-		'.csv'
-	]
+    # list of enpoints to be used to access data included with product
+    data_endpoints = [
+        '.json',
+        '.csv'
+    ]
 
-	# multithreading speed up accessing data, making lambda run quicker
-	with (Pool(2)) as p:
-		asset_list = p.map(data_to_s3, data_endpoints)
+    # multithreading speed up accessing data, making lambda run quicker
+    with (Pool(2)) as p:
+        asset_list = p.map(data_to_s3, data_endpoints)
 
-	# asset_list is returned to be used in lamdba_handler function
-	return asset_list
+    # asset_list is returned to be used in lamdba_handler function
+    return asset_list
+
+
 source_dataset()
